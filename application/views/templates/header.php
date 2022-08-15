6c82658d938199bb4a0b88c45ee5b1dab7e20a0a
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2022-08-15 09:10:58 +0800

    change favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="<?= base_url('assets/img/logo_rinjani.png') ?>">
+    <link rel="icon" href="<?= base_url('assets/img/favicon.ico') ?>">
     <title><?= $title; ?></title>
     <!-- Font Awesome Versi 5 -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" type="text/css">
